f4c75299582900e1cf9967bade6c1153da3003b4
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2023-11-26 19:03:48 -0600

    added departments

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,14 +2,16 @@ import { Router } from 'express'
 
 import persons from './persons'
 import companies from './companies'
+import departments from './departments'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send({ msg: 'Inside API Endpoints' })
-})  
+  res.send({ msg: 'Inside API Endpoints' })
+})
 
 router.use('/persons', persons)
 router.use('/companies', companies)
+router.use('/departments', departments)
 
 export default router
